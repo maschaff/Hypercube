e421b7a1c9736380f56ae92dd4113b009d69bf6f
maschaff <schaffer.mb@gmail.com>
2019-12-03 17:18:26 -0600

    Updating to show current strategy for projection and rotation

--- hypercube.js
+++ hypercube.js
@@ -1,80 +1,152 @@
-var vertices = [
-    new THREE.Vector3(-1, -1, 1),
-    new THREE.Vector3(1, -1, 1),
-    new THREE.Vector3(-1, 1, 1),
-    new THREE.Vector3(1, 1, 1),
-    new THREE.Vector3(-1, -1, -1),
-    new THREE.Vector3(1, -1, -1),
-    new THREE.Vector3(-1, 1, -1),
-    new THREE.Vector3(1, 1, -1)
+let vertices = [
+    new THREE.Vector4(-1, -1, -1, 1),
+    new THREE.Vector4(1, -1, -1, 1),
+    new THREE.Vector4(1, 1, -1, 1),
+    new THREE.Vector4(-1, 1, -1, 1),
+    new THREE.Vector4(-1, -1, 1, 1),
+    new THREE.Vector4(1, -1, 1, 1),
+    new THREE.Vector4(1, 1, 1, 1),
+    new THREE.Vector4(-1, 1, 1, 1),
+    new THREE.Vector4(-1, -1, -1, -1),
+    new THREE.Vector4(1, -1, -1, -1),
+    new THREE.Vector4(1, 1, -1, -1),
+    new THREE.Vector4(-1, 1, -1, -1),
+    new THREE.Vector4(-1, -1, 1, -1),
+    new THREE.Vector4(1, -1, 1, -1),
+    new THREE.Vector4(1, 1, 1, -1),
+    new THREE.Vector4(-1, 1, 1, -1)
 ];
 
+let edges = [
+    //inner cube
+    [0, 1], [1, 2], [2, 3], [3, 0],
+    [4, 5], [5, 6], [6, 7], [7, 4],
+    [0, 4], [1, 5], [2, 6], [3, 7],
 
-function main() {
+    //outer cube
+    [8, 9], [9, 10], [10, 11], [11, 8],
+    [12, 13], [13, 14], [14, 15], [15, 12],
+    [8, 12], [9, 13], [10, 14], [11, 15],
 
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    //connecting planes
+    [0, 8], [1, 9],
+    [2, 10], [3, 11],
+    [4, 12], [5, 13],
+    [6, 14], [7, 15]
+];
 
-    var renderer = new THREE.WebGLRenderer();
+let angle = 0;
+let distance = 1.5;
+
+let scene;
+let camera;
+let renderer;
+let material;
+
+let lines = [];
+
+function init(){
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer();
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    // var cube = new THREE.Mesh( geometry, material );
-    // scene.add( cube );
+    material = new THREE.LineBasicMaterial({color: 0x0000ff});
 
-    var material = new THREE.LineBasicMaterial({color: 0x0000ff});
+    camera.position.z = 8;
 
-    var object = new THREE.Geometry(1, 1, 1);
+    //generate initial geometry
+    for(let i = 0; i < 2; i++){
+        let geometry = new THREE.Geometry();
+        geometry.vertices.push(get3dProjection(vertices[edges[i][0]]));
+        geometry.vertices.push(get3dProjection(vertices[edges[i][1]]));
+        lines.push(new THREE.Line(geometry, material));
+        scene.add(lines[i]);
+    }
+}
 
-    object.vertices.push(
-        new THREE.Vector3(-1, -1, 1),
-        new THREE.Vector3(1, -1, 1),
-        new THREE.Vector3(-1, 1, 1),
-        new THREE.Vector3(1, 1, 1),
-        new THREE.Vector3(-1, -1, -1),
-        new THREE.Vector3(1, -1, -1),
-        new THREE.Vector3(-1, 1, -1),
-        new THREE.Vector3(1, 1, -1)
-    );
+function main(){
 
-    object.faces.push(
-        new THREE.Face3(0, 3, 2),
-        new THREE.Face3(0, 1, 3),
+    init();
 
-        new THREE.Face3(1, 7, 3),
-        new THREE.Face3(1, 5, 7),
-
-        new THREE.Face3(5, 6, 7),
-        new THREE.Face3(5, 4, 6),
-
-        new THREE.Face3(4, 2, 6),
-        new THREE.Face3(4, 0, 2),
-
-        new THREE.Face3(2, 7, 6),
-        new THREE.Face3(2, 3, 7),
-
-        new THREE.Face3(4, 1, 0),
-        new THREE.Face3(4, 5, 1)
-    );
-
-    var edges = new THREE.EdgesGeometry(object);
-
-    var wireframe = new THREE.LineSegments(edges, material);
-    scene.add(wireframe);
-
-    camera.position.z = 5;
-
-    function animate() {
+    function animate(){
         requestAnimationFrame(animate);
-        wireframe.rotation.x += 0.01;
-        wireframe.rotation.y += 0.01;
+
+        // angle += 5;
+        //
+        // rotate();
+        // updateGeometry();
 
         renderer.render(scene, camera);
     }
 
     animate();
+}
 
+function rotate(){
+    let rotation = new THREE.Matrix4();
 
+    //double rotation matrix in xy and zw planes
+    // rotation.set(
+    //     Math.cos(toRadians(angle)), -1 * Math.sin(toRadians(angle)), 0, 0,
+    //     Math.sin(toRadians(angle)), Math.cos(toRadians(angle)), 0, 0,
+    //     0, 0, Math.cos(toRadians(angle)), -1 * Math.sin(toRadians(angle)),
+    //     0, 0, Math.sin(toRadians(angle)), Math.cos(toRadians(angle))
+    // );
+
+    //rotation matrix in the xy plane
+    rotation.set(
+        Math.cos(toRadians(angle)), -1 * Math.sin(toRadians(angle)), 0, 0,
+        Math.sin(toRadians(angle)), Math.cos(toRadians(angle)), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    );
+
+    //rotation matrix in the zw plane
+    // rotation.set(
+    //     1, 0, 0, 0,
+    //     0, 1, 0, 0,
+    //     0, 0, Math.cos(toRadians(angle)), -1 * Math.sin(toRadians(angle)),
+    //     0, 0, Math.sin(toRadians(angle)), Math.cos(toRadians(angle))
+    // );
+
+    for(let i = 0; i < vertices.length; i++){
+        vertices[i] = vertices[i].applyMatrix4(rotation);
+    }
+}
+
+function updateGeometry(){
+
+    for(let i = 0; i < lines.length; i++){
+        lines[i].geometry.vertices[0] = get3dProjection(vertices[edges[i][0]]);
+        lines[i].geometry.vertices[1] = get3dProjection(vertices[edges[i][1]]);
+        // lines[i].rotation.x = 0;
+        // lines[i].rotation.y = 0;
+        // lines[i].rotation.z = 0;
+        lines[i].geometry.verticesNeedUpdate = true;
+    }
+}
+
+function get3dProjection(v){
+
+    //stereographic projection matrix
+    let projection = new THREE.Matrix4();
+    let w = 1 / (distance - v.w);
+
+    projection.set(
+        w, 0, 0, 0,
+        0, w, 0, 0,
+        0, 0, w, 0,
+        0, 0, 0, 0
+    );
+
+    let result = v.applyMatrix4(projection);
+
+    return new THREE.Vector3(result.x, result.y, result.z);
+}
+
+function toRadians(angle){
+    return Math.PI * angle / 180;
 }
